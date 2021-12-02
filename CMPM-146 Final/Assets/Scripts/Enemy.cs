4ea676acf858187f90d2fd740e71eda54ceee339
gssaini117 <gssaini@ucsc.edu>
2021-12-01 21:00:12 -0800

    Added Gun + Shooting functionality

---
+++ CMPM-146 Final/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
