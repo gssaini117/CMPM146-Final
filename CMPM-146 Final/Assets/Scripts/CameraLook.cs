37daf280dc359be4b8f04912ea62f8caafdea469
gssaini117 <gssaini@ucsc.edu>
2021-12-01 20:21:56 -0800

    Implemented First Person Camera

---
+++ CMPM-146 Final/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("MouseX") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("MouseY") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
